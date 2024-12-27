2e356afeb3a36b85ae5ff6fef0661632ee748f5d
adamjsturge <adamjsturge@gmail.com>
2024-12-27 13:20:20 -0800

    Npm package part 1

---
+++ json/index.js
@@ -0,0 +1,9 @@
+const all = require('one-piece-card-game-json-all');
+const en = require('one-piece-card-game-json-en');
+const jp = require('one-piece-card-game-json-jp');
+
+module.exports = {
+  all,
+  en,
+  jp
+};
